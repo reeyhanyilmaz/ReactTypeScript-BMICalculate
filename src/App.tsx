231b0ebcb0ed34174a11631191c16b2a9bd3d337
Reyhan Yılmaz <reyhanylmz1993@gmail.com>
2022-07-19 19:25:23 +0300

    add routes and components

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
-import React from 'react';
-import './App.scss';
-import Navbar from './components/Navbar/';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import "./App.scss";
+import CalculateData from "./components/CalculateData";
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+import WhatIsBMI from "./components/WhatIsBMI";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
 
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/whatisBMI" element={<WhatIsBMI />} />
+        <Route path="/calculate-data" element={<CalculateData />} />
+      </Routes>
     </div>
   );
 }
