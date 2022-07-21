f614b204f59d7e488c40926439ab84290361fac3
Reyhan Yılmaz <reyhanylmz1993@gmail.com>
2022-07-21 18:01:50 +0300

    add input icon

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,108 +1,135 @@
-import React , {useState} from 'react';
-import './style.scss';
-import styled from 'styled-components';
+import React, { useState } from "react";
+import "./style.scss";
+import styled from "styled-components";
 
 function Home() {
- 
   const Button = styled.button`
-  color: rgb(180, 114, 180);
-  font-size: 1em;
-  margin: 1em;
-  padding: 0.25em 1em;
-  border: 2px solid rgb(180, 114, 180);
-  border-radius: 8px;
-  cursor: pointer;
+    color: rgb(180, 114, 180);
+    font-size: 1em;
+    margin: 1em;
+    padding: 0.25em 1em;
+    border: 2px solid rgb(180, 114, 180);
+    border-radius: 8px;
+    cursor: pointer;
 
-  &:hover{
-    background-color: rgb(180, 114, 180);
-    color: white;
-  }
-`;
+    &:hover {
+      background-color: rgb(180, 114, 180);
+      color: white;
+    }
+  `;
 
-const Swal = require('sweetalert2');
+  const Swal = require("sweetalert2");
 
-const handleSubmit = (e:any) => {
-  e.preventDefault();     
-}
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+  };
 
+  const [name, setName] = useState<string>("");
+  const [weight, setWeight] = useState<number>(0);
+  const [height, setHeight] = useState<number>(0);
+  const [BMI, setBMI] = useState<number>();
+  const [localData, setLocalData] = useState<any>([]);
 
-const [name, setName] = useState<string>("");
-const [weight, setWeight] = useState<number>(0);
-const [height, setHeight] = useState<number>(0);
-const [BMI, setBMI] = useState<number>();
-const [localData, setLocalData] = useState<any>([]);
+  const calculate = () => {
+    const BMICalculate = weight / (height / 100) ** 2;
+    setBMI(BMICalculate);
 
-const calculate = () => {
+    localData.push({
+      name: name,
+      BMI: BMICalculate,
+    });
+    setLocalData(localData);
 
-      const BMICalculate = (weight / (height / 100) ** 2);
-      setBMI(BMICalculate);
+    const handleStorage = () => {
+      localStorage.setItem("userData", JSON.stringify([...localData]));
+      JSON.parse(localStorage.getItem("userData") || "{}"); //  bu kısım || type error verdiği için.
+    };
+    handleStorage();
 
-      localData.push({
-        name: name,
-        BMI: BMICalculate,
-      })
-      setLocalData(localData);
-
-      const handleStorage = () => {
-        localStorage.setItem('userData', JSON.stringify([...localData]));
-        JSON.parse(localStorage.getItem('userData') || '{}'); //  bu kısım || type error verdiği için.
-      }
-      handleStorage();
-
-     const status = () => {
+    const status = () => {
       if (BMICalculate < 18.5) {
-        return "Zayıf" }
-      else if (BMICalculate >= 18.5 && BMICalculate < 25) {
-        return  "Normal" }
-      else if (BMICalculate >= 25 && BMICalculate < 30) {
-        return "Fazla Kilolu" }
-      else if (BMICalculate >= 30) {
-        return "Obez" }
-      }   
-     status();
+        return "Zayıf";
+      } else if (BMICalculate >= 18.5 && BMICalculate < 25) {
+        return "Normal";
+      } else if (BMICalculate >= 25 && BMICalculate < 30) {
+        return "Fazla Kilolu";
+      } else if (BMICalculate >= 30) {
+        return "Obez";
+      }
+    };
+    status();
 
-
-      Swal.fire({
-        title: `${status()}`,
-        text: `${name} BMI değerin: ${BMICalculate}`,
-        width: 600,
-        padding: '3em',
-        color: '#716add',
-        background: '#fff url(/images/trees.png)',
-        confirmButtonText: 'Diyet programına git',
-        confirmButtonColor: 'rgb(180, 114, 180)',
-        backdrop: `
+    Swal.fire({
+      title: `${status()}`,
+      text: `${name} BMI değerin: ${BMICalculate}`,
+      width: 600,
+      padding: "3em",
+      color: "#716add",
+      background: "#fff url(/images/trees.png)",
+      confirmButtonText: "Diyet programına git",
+      confirmButtonColor: "rgb(180, 114, 180)",
+      backdrop: `
           rgba(0,0,123,0.4)
           url("/images/nyan-cat.gif")
           left top
           no-repeat
-        `       
-      }).then((confirmButton:any) => {
-        if (confirmButton.value) {
-          window.location.href = '/diet-list';
-        }})
-      
-      setName("");
-      setWeight(0);
-      setHeight(0); 
-    }
+        `,
+    }).then((confirmButton: any) => {
+      if (confirmButton.value) {
+        window.location.href = "/diet-list";
+      }
+    });
+
+    setName("");
+    setWeight(0);
+    setHeight(0);
+  };
 
   return (
-    <div className='home-container'>
+    <div className="home-container">
       <div>
-        <img src="../assets/home.png" alt="" />
+        <img src="../assets/home.png" alt="homeimage" className="homeimage"/>
       </div>
 
       <div>
-        <form  onChange={handleSubmit}>        
-          <input type="text" placeholder="İsminizi giriniz..."  onChange={(e) => setName(e.target.value)}/>
-          <input type="number" placeholder="Kilonuzu giriniz, örn 50kg"  onChange={(e) => setWeight(parseInt(e.target.value))}/>
-          <input type="number" placeholder="Boyunuzu giriniz, örn 160cm"  onChange={(e) => setHeight(parseInt(e.target.value))}/>
-          <Button onClick={calculate}>Hesapla</Button>
+        <form onChange={handleSubmit}>
+          <span>
+            <img src="./assets/name.png" alt="name" />
+            <input
+              type="text"
+              placeholder="İsminizi giriniz..."
+              onChange={(e) => setName(e.target.value)}
+            />
+          </span>
+
+          <span>
+            <img src="./assets/kg.png" alt="kg" />
+            <input
+              type="number"
+              placeholder="Kilonuzu giriniz, örn 50kg"
+              onChange={(e) => setWeight(parseInt(e.target.value))}
+            />
+          </span>
+
+          <span>
+            <img src="./assets/height.png" alt="cm" />
+            <input
+              type="number"
+              placeholder="Boyunuzu giriniz, örn 160cm"
+              onChange={(e) => setHeight(parseInt(e.target.value))}
+            />
+          </span>
+          <Button onClick={ name && weight && height ? calculate : Swal.fire({
+                icon: 'error',
+                title: 'Oops...',
+                text: 'İnput alanlarını boş geçemezsiniz!',
+                showConfirmButton: false,
+                timer: 2000,
+              })}>Hesapla</Button>
         </form>
       </div>
     </div>
-  )
+  );
 }
 
 export default Home;
