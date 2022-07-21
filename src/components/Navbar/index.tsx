7545c25d8cb6a18bb32f13925c9e8268152ade72
Reyhan Yılmaz <reyhanylmz1993@gmail.com>
2022-07-21 17:00:54 +0300

    add Navbar icon

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -6,20 +6,20 @@ function Navbar() {
   return (
     <div className="navbar">
       <span>
-        <img src="./assets/header.png" alt="logo" />
+        <img src="./assets/header.png" alt="logo" className="nav-img"/>
       </span>
 
       <span>
         <ul>
           <li>
-            <Link to="/whatisBMI">BMI Nedir</Link>
+            <Link to="/whatisBMI"> <img src="./assets/question.png" alt="question" />BMI Nedir? </Link>
           </li>
           <li>
-            <Link to="/">BMI Hesapla</Link>
+            <Link to="/"> <img src="./assets/calculator.png" alt="calculate" />BMI Hesapla</Link>
           </li>
 
           <li>
-            <Link to="/calculate-data">Verilerim</Link>
+            <Link to="/diet-list"><img src="./assets/checklist.png" alt="list" />Diyet Listesi </Link>
           </li>
         </ul>
       </span>
